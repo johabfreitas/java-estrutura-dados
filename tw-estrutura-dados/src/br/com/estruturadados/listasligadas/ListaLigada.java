4c8f046d6b3898ccc27e923e226b6da1cf2d1b4d
Johab <freitasjohab@gmail.com>
2022-07-04 22:36:25 -0300

    Listas Ligadas

---
+++ tw-estrutura-dados/src/br/com/estruturadados/listasligadas/ListaLigada.java
@@ -0,0 +1,14 @@
+package br.com.estruturadados.listasligadas;
+
+public class ListaLigada<T> {
+
+	private No<T> primeiroNo;
+	private No<T> ultimoNo;
+	private int tamanho;
+	
+	public ListaLigada(){
+		this.primeiroNo = null;
+		this.ultimoNo = null;
+	}
+	
+}
